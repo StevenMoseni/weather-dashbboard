7cf8390de5acf1102b78d371ef63954af0ca9711
Steven Moseni <smoseni@hotmail.com>
2021-03-01 13:30:43 -0800

    add event click functions

--- assets/scipt.js
+++ assets/scipt.js
@@ -8,3 +8,10 @@ var currentHumidty= $("#humidity");
 var currentWSpeed=$("#wind-speed");
 var currentUvindex= $("#uv-index");
 var sCity=[];
+
+
+
+$("#search-button").on("click",displayWeather);
+$(document).on("click",invokePastSearch);
+$(window).on("load",loadlastCity);
+$("#clear-history").on("click",clearHistory);
